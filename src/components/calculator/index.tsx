7250da5917ee7f11a08afdd68508868e3246d2d9
R72\User <m4rkusfletcher@gmail.com>
2023-05-19 19:42:13 +0300

    calc form

--- src/components/calculator/index.tsx
+++ src/components/calculator/index.tsx
@@ -1,21 +1,86 @@
-import React, { useState, ReactElement } from 'react'
+import React, { useEffect, useState, ReactElement } from 'react'
 import s from './calculator.module.scss'
 
-import { FormField } from '../ui/form-field'
-import { InputText } from '../ui/input-text'
+import { ITransportationData } from '../../types/transportation'
 
-interface FormData {
-  from: string,
-  to: string
+import { FormField } from '../ui/form-field'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
+import { Distance } from '../../service/distance'
+import { Cars } from '../../service/cars'
+
+const formDataDefault: ITransportationData = {
+  from: '',
+  to: '',
+  date: '',
+  time: '',
+  duration: '',
+  movers: 0,
+  passengers: 0
 }
 
 export const Calculator: React.FC = (): ReactElement => {
-  const [formData, setFormData] = useState<FormData>({} as FormData)
+  const [formData, setFormData] = useState<ITransportationData>(formDataDefault)
+  const [distance, setDistance] = useState<number>(0)
+  const [price, setPrice] = useState<number>(0)
+
+  const formInputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const name: string = e.target.name
+    const value: string = e.target.value
+    setFormData(prev => ({...prev, [name]: value}))
+    console.log(formData)
+  }
+
+  useEffect(() => {
+    Distance.calculate(formData.from, formData.to).then((distance) => {
+      setDistance(distance)
+    })
+  }, [formData.from, formData.to])
+
+  useEffect(() => {
+    Cars.get().then(res => {
+      console.log(res)
+    })
+  }, [])
+
   return (
     <form className={s.calc} action="">
-      <FormField label='Откуда' forId="1">
-        {/* <InputText id="from" name="from" value={formData} onChange={() => setFrom}></InputText> */}
-      </FormField>
+      <h1>Расчет стоимости перевозки</h1>
+      <div className={s.fields}>
+        <FormField label='Откуда' forId="from">
+          {<Input id="from" name="from" value={formData.from} onChange={formInputHandler}></Input>}
+        </FormField>
+        <FormField label='Куда' forId="to">
+          {<Input id="to" name="to" value={formData.to} onChange={formInputHandler}></Input>}
+        </FormField>
+        <div className={s.timeFields}>
+          <FormField label='Дата' forId="date">
+            {<Input id="date" name="date" type='date' value={formData.date} onChange={formInputHandler}></Input>}
+          </FormField>
+          <FormField label='Время' forId="time">
+            {<Input id="time" name="time" type='time' value={formData.time} onChange={formInputHandler}></Input>}
+          </FormField>
+          <FormField label='Длительнось' forId="duration">
+            {<Input id="duration" name="duration" type='time' value={formData.duration} onChange={formInputHandler}></Input>}
+          </FormField>
+        </div>
+        <div className={s.personsFields}>
+          <FormField label='Грузчики' forId="movers">
+            {<Input id="movers" name="movers" type='number' value={formData.movers} onChange={formInputHandler}></Input>}
+          </FormField>
+          <FormField label='Пассажиры' forId="passengers">
+            {<Input id="passengers" name="passengers" type='number' value={formData.passengers} onChange={formInputHandler}></Input>}
+          </FormField>
+        </div>
+      </div>
+      <div className={s.price}>
+        <span className={s.priceLabel}>Итого</span>
+        <span>{ price }</span>
+      </div>
+      <div className={s.actions}>
+        <Button text="Оформить"/>
+      </div>
     </form>
   )
 }
